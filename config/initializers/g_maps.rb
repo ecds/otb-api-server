6274da421678d2102667aeeca77f8a960163b2e6
Jay Varner <jayvarner@gmail.com>
2021-09-09 09:55:31 -0400

    Remove debug statements. Add fake Google key for testing.

--- config/initializers/g_maps.rb
+++ config/initializers/g_maps.rb
@@ -1,3 +1,7 @@
 GoogleMapsService.configure do |config|
-  config.key = Rails.application.credentials.dig(:g_maps_key)
+  if ENV['RAILS_ENV'] == 'test'
+    config.key = 'FAkeFaK-E_fAkeChv-P3nchtQYHoCLfFzn9ylr8'
+  else
+    config.key = Rails.application.credentials.dig(:g_maps_key)
+  end
 end

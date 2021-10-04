40c82c5f6abbaa1c220d0dd81eb8902e15325c40
Jay Varner <jayvarner@gmail.com>
2021-10-04 13:11:50 -0400

    Clean up

--- app/services/google_directions.rb
+++ app/services/google_directions.rb
@@ -16,22 +16,16 @@ class GoogleDirections
       "https://maps.googleapis.com/maps/api/distancematrix/json?#{@query.to_query}"
     ).with_indifferent_access
 
-    retun nil if response[:rows].first[:elements].first[:status] == 'ZERO_RESULTS'
+    return nil if response[:rows].first[:elements].first[:status] == 'ZERO_RESULTS'
 
     response
   end
 
   def durations
-    matrix[:rows].first[:elements].map { |e| e[:duration][:value] if e[:duration].present? }.reject { |d| d.nil? }
+    matrix.nil? ? nil : matrix[:rows].first[:elements].map { |e| e[:duration][:value] if e[:duration].present? }.reject { |d| d.nil? }
   end
 
   def duration
-    durations.sum + 600 + (@stops_count * 600)
+    durations.nil? ? nil : durations.sum + 600 + (@stops_count * 600)
   end
 end
-
-# Apartment::Tenant.switch! 'july-22nd'
-# t = Tour.first
-# destinations = t.tour_stops.order(:position).map { |tour_stop| [tour_stop.stop.lat, tour_stop.stop.lng] }
-# origin = destinations.shift
-# gmap = GoogleDirections.new(origin, destinations)

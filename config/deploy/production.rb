8a99a26125ccf7badcc14deb0ca776179b64ebbb
Jay Varner <jayvarner@gmail.com>
2022-07-29 11:32:59 -0400

    Update deploy config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 set :branch, 'develop'
 
 # server '44.192.30.237', user: 'deploy', roles: %w{app db web}, primary: :my_value
-role :app, %w{34.239.167.5 44.201.150.24}, user: 'deploy'
-role :web, %w{34.239.167.5 44.201.150.24}, user: 'deploy'
-role :db,  %w{34.239.167.5 44.201.150.24}, user: 'deploy'
+role :app, %w{34.239.167.5 44.202.38.154}, user: 'deploy'
+role :web, %w{34.239.167.5 44.202.38.154}, user: 'deploy'
+role :db,  %w{34.239.167.5 44.202.38.154}, user: 'deploy'
 
 set :deploy_to, '/data/otb-api-server'
 

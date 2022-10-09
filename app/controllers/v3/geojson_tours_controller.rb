7808081dc148b02fd8a12a307336597062b9ed5b
Jay Varner <jayvarner@gmail.com>
2022-10-09 13:47:12 -0400

    Update GeoJSON view

--- app/controllers/v3/geojson_tours_controller.rb
+++ app/controllers/v3/geojson_tours_controller.rb
@@ -5,30 +5,58 @@
 #
 module V3
   class GeojsonToursController < ApplicationController
+    include ActionView::Helpers::SanitizeHelper
+
     def show
       @tour = Tour.find(params[:id])
-      if @tour.published
-        render json: { type: 'FeatureCollection', features: @tour.stops.map { |s| feature(s) } }.to_json
-      else
-        head 401
-      end
+      # if @tour.published
+      geojosn = {
+                  type: 'FeatureCollection',
+                  crs: {
+                    type: 'name',
+                    properties: {
+                      name: 'urn:ogc:def:crs:EPSG::4326'
+                    }
+                  },
+                  meta: meta_content,
+                  features: @tour.tour_stops.map { |tour_stop| feature(tour_stop.position, tour_stop.stop) }
+                }
+      render json: geojosn.to_json
+      # render json: { type: 'FeatureCollection', meta: meta_content, features: @tour.tour_stops.map { |tour_stop| feature(tour_stop.position, tour_stop.stop) } }.to_json
+      # else
+      #   head 401
+      # end
     end
 
     private
 
-      def feature(stop)
+      def meta_content
+        {
+          title: @tour.title,
+          intro: sanitize(@tour.description)
+        }
+      end
+
+      def feature(position, stop)
         stop.media.map { |m| m.caption = nil if m.caption.blank? }
         {
           type: 'Feature',
           geometry: {
-              type: 'Point',
-              coordinates: [stop.lng.to_f, stop.lat.to_f]
-            },
-            properties: {
-              title: stop.title,
-              description: stop.description,
-              images: stop.media.map { |m| { caption: m.caption, url: "#{request.protocol}#{request.host}/#{m.desktop}" } }
-            }
+            type: 'Point',
+            coordinates: [stop.lng.to_f, stop.lat.to_f]
+          },
+          properties: {
+            title: stop.title,
+            description: stop.description,
+            position: position,
+            images: stop.media.map do |m|
+              {
+                caption: m.caption,
+                full: m.files[:desktop],
+                thumb: m.files[:mobile]
+              }
+            end
+          }
         }
       end
   end

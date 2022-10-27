e0af65ff1584db6e867c6198614f470fc949a37c
Jay Varner <jayvarner@gmail.com>
2022-10-27 09:42:25 -0400

    Update Prod IPs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 set :branch, 'develop'
 
 # server '44.192.30.237', user: 'deploy', roles: %w{app db web}, primary: :my_value
-role :app, %w{34.239.167.5 44.202.38.154}, user: 'deploy'
-role :web, %w{34.239.167.5 44.202.38.154}, user: 'deploy'
-role :db,  %w{34.239.167.5 44.202.38.154}, user: 'deploy'
+role :app, %w{3.86.138.59 3.236.252.227}, user: 'deploy'
+role :web, %w{3.86.138.59 3.236.252.227}, user: 'deploy'
+role :db,  %w{3.86.138.59 3.236.252.227}, user: 'deploy'
 
 set :deploy_to, '/data/otb-api-server'
 

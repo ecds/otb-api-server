5be734dae2a5a525138850b724be1e03523952b1
Jay Varner <jayvarner@gmail.com>
2022-10-28 08:47:04 -0400

    Allow CORS for OpenWorld

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://lvh.me:4200', 'https://otb.ecdsdev.org', 'https://opentour.site', /.*\.opentour.site/, /.*\.lvh.me:4200/
+    origins 'https://lvh.me:4200', 'https://otb.ecdsdev.org', 'https://opentour.site', /.*\.opentour.site/, /.*\.lvh.me:4200/, /.*\.localhost:3000/, /.*\.urbanspatialhistory.org/
 
     resource '*',
              headers: :any,

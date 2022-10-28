51561891a1705af19d4d62d4399a8a24ff13d3ec
Jay Varner <jayvarner@gmail.com>
2022-10-28 08:37:39 -0400

    Extend life of ActiveStorage objects

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -8,4 +8,4 @@
 # })
 
 # Sometimes the service URL expires too quickly.
-Rails.application.config.active_storage.service_urls_expire_in = 1.hour
+Rails.application.config.active_storage.service_urls_expire_in = 1.week

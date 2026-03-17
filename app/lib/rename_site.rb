bdb78140274269bfab92879575506755cfdf57e6
Jay Varner <jayvarner@gmail.com>
2026-03-17 11:21:01 -0400

    Add dependencies to workflow

--- app/lib/rename_site.rb
+++ app/lib/rename_site.rb
@@ -1,12 +1,14 @@
-def rename_site(old_name, new_name)
-  Apartment::Tenant.switch! "public"
-  ActiveRecord::Base.transaction do
-    # 1. Rename the schema
-    ActiveRecord::Base.connection.execute(
-      "ALTER SCHEMA \"#{old_name}\" RENAME TO \"#{new_name}\""
-    )
+class RenameSite
+  def rename_site(old_name, new_name)
+    Apartment::Tenant.switch! "public"
+    ActiveRecord::Base.transaction do
+      # 1. Rename the schema
+      ActiveRecord::Base.connection.execute(
+        "ALTER SCHEMA \"#{old_name}\" RENAME TO \"#{new_name}\""
+      )
 
-    # 2. Update the tenant record
-    TourSite.find_by!(subdir: old_name).update!(subdomain: new_name)
+      # 2. Update the tenant record
+      TourSite.find_by!(subdir: old_name).update!(subdomain: new_name)
+    end
   end
 end
